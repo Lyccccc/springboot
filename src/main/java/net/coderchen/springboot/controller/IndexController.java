6518466d73555edbaf0ab770ef78646799837731
Liuyuchen <472779948@qq.com>
2018-11-21 22:03:15 +0800

    添加jsp支持

---
+++ src/main/java/net/coderchen/springboot/controller/IndexController.java
@@ -0,0 +1,17 @@
+package net.coderchen.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author coderchen
+ * 2018/11/21 22:00
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
